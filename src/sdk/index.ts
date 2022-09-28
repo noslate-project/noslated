e554f5cac9555164f2e6e72c59317330760da763
mariodu <dujiakun@gmail.com>
2022-09-28 09:49:29 +0000

    chore: export sdk as noslated client

--- src/sdk/index.ts
+++ src/sdk/index.ts
@@ -2,6 +2,7 @@ import { AliceClient } from './client';
 import { CanonicalCode } from '#self/delegate/index';
 
 export {
+  AliceClient as NoslatedClient,
   AliceClient as AliceAgent,
   AliceClient,
   CanonicalCode,
