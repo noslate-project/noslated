93f4ca308409145eafdafef3f5450d61fa931a76
mariodu <dujiakun@gmail.com>
2023-04-19 06:53:26 +0000

    chore: remove useless

--- fixtures/dapr_adaptor/index.js
+++ fixtures/dapr_adaptor/index.js
@@ -1,17 +1,5 @@
 'use strict';
 
-// module.exports = class MockDaprAdaptor {
-//   isReady = false;
-
-//   async ready() {
-//     this.isReady = true;
-//   }
-
-//   async close() {
-//     this.isReady = false;
-//   }
-// }
-
 const { Base } = require('#self/lib/sdk_base');
 
 class DaprAdaptor extends Base {
